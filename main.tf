9419cf24ef2d8483644dbab1a5f6e85b12d35d2c
Krishna <charan.nithinj@gmail.com>
2022-09-16 19:07:59 +0530

    Updated

--- main.tf
+++ main.tf
@@ -20,10 +20,10 @@ resource "azuread_user" "users" {
   for_each = { for user in local.users : user.first_name => user }
 
   user_principal_name = format(
-    "%s%s-%s@%s",
+    "%s%s%s@%s",
     substr(lower(each.value.first_name), 0, 1),
     lower(each.value.last_name),
-    random_pet.suffix.id,
+    "",
     local.domain_name
   )
 
