ad6a65b06c49e73f18b05a95e4ecb1bd8933629f
Krishna <charan.nithinj@gmail.com>
2022-09-18 09:16:18 +0530

    Updated

--- versions.tf
+++ versions.tf
@@ -5,4 +5,10 @@ terraform {
       version = "2.0.1"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate14662"
+    container_name       = "tfstate"
+    key                  = "test.terraform.tfstate"
+  }
 }
